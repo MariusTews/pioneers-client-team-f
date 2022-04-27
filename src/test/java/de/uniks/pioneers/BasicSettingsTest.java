5bf08d6f22ad8ac2fa989e3ae5b060b0e8ff33e4
Philipp L <p.leibl@gmx.de>
2022-04-27 21:38:36 +0200

    test: App Icons

--- src/test/java/de/uniks/pioneers/BasicSettingsTest.java
+++ src/test/java/de/uniks/pioneers/BasicSettingsTest.java
@@ -1,9 +1,14 @@
 package de.uniks.pioneers;
 
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import javax.imageio.ImageIO;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BasicSettingsTest extends ApplicationTest {
@@ -22,5 +27,29 @@ public class BasicSettingsTest extends ApplicationTest {
     @Test
     public void initialTest() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testWindowIcon() {
+
+        Image image1 = new Image(App.class.getResource("FATARI_logo.png").toString());
+        Image image2 = stage.getIcons().get(0);
+
+        for (int i = 0; i < image1.getWidth(); i++) {
+            for (int j = 0; j < image1.getHeight(); j++) {
+
+                assertEquals(image1.getPixelReader().getColor(i, j), image2.getPixelReader().getColor(i, j));
+            }
+        }
+    }
+
+    @Test
+    public void testTaskIcon() {
+        try {
+            java.awt.Image image = ImageIO.read(Objects.requireNonNull(Main.class.getResource("FATARI_logo.png")));
+            assertEquals(java.awt.Taskbar.getTaskbar().getIconImage(), image);
+        }
+        catch (Exception ignored) {
+        }
     }
 }
