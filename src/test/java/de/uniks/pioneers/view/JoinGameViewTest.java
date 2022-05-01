f7048f5203017cc603f3656b3bafeea9ab0c1f0b
MWrab <mike.wohlrab@live.de>
2022-05-01 14:20:40 +0200

    test: joinGame view elements

---
+++ src/test/java/de/uniks/pioneers/view/JoinGameViewTest.java
@@ -0,0 +1,48 @@
+package de.uniks.pioneers.view;
+
+import de.uniks.pioneers.App;
+import de.uniks.pioneers.controller.JoinGameController;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class JoinGameViewTest extends ApplicationTest {
+
+    private Stage stage;
+    private App app;
+
+    @Override
+    public void start(Stage stage) {
+        // start application
+        this.stage = stage;
+        this.app = new App();
+        this.app.start(stage);
+    }
+
+    @Test
+    public void testViewParameters()
+    {
+        //opens the Lobby to test the View without the need to Login
+        app.show(new JoinGameController());
+
+        Button backButtonJoinGame = lookup("#backButtonJoinGame").query();
+        Button joinButton = lookup("#joinButton").query();
+
+        TextField passwordTextField = lookup("#passwordTextField").query();
+
+        Assertions.assertThat(backButtonJoinGame.getText()).isEqualTo("Back");
+        Assertions.assertThat(joinButton.getText()).isEqualTo("Join");
+
+        Assertions.assertThat(passwordTextField.getText()).isEqualTo("");
+
+        clickOn(passwordTextField);
+        write("test");
+        Assertions.assertThat(passwordTextField.getText()).isEqualTo("test");
+
+        clickOn(backButtonJoinGame);
+        clickOn(joinButton);
+
+    }
+}
