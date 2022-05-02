79b64960d9df45fc7e95fb696c012bdd4e78d28a
JanicEbermann <Ebermann.extra@gmx.de>
2022-05-02 22:27:34 +0200

    test: signUp-, rules- view

---
+++ src/test/java/de/uniks/pioneers/view/RulesScreenViewTest.java
@@ -0,0 +1,20 @@
+package de.uniks.pioneers.view;
+
+import de.uniks.pioneers.App;
+import javafx.stage.Stage;
+import org.testfx.framework.junit5.ApplicationTest;
+
+class RulesScreenViewTest extends ApplicationTest {
+    private Stage stage;
+    private App app;
+
+    @Override
+    public void start(Stage stage) {
+        // start application
+        this.stage = stage;
+        this.app = new App();
+        this.app.start(stage);
+    }
+
+
+}
