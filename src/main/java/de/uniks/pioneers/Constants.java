8d2beccc465b43c2c570810da033eaf94976fcda
MWrab <mike.wohlrab@live.de>
2022-05-03 15:45:10 +0200

    fix: Constants typo

--- src/main/java/de/uniks/pioneers/Constants.java
+++ src/main/java/de/uniks/pioneers/Constants.java
@@ -3,11 +3,11 @@ package de.uniks.pioneers;
 public class Constants {
 
     //Network
-    public static final String BASE_URL = "https://pioneers.uniks.de";
+    public static final String WS_URL = "wss://pioneers.uniks.de";
 
     public static final String WS_PREFIX = "/ws";
     public static final String VERSION_PREFIX = "/v1";
     public static final String WS_AUTHTOKEN_PREFIX = "/events?authToken=";
 
-    public static final String WS_AUTHTOKEN_URL = BASE_URL + WS_PREFIX + VERSION_PREFIX + WS_AUTHTOKEN_PREFIX;
+    public static final String WS_AUTHTOKEN_URL = WS_URL + WS_PREFIX + VERSION_PREFIX + WS_AUTHTOKEN_PREFIX;
 }
