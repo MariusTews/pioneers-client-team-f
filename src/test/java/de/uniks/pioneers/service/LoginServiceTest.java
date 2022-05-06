71621697e669e39f9563273482e0f7f0ba34b4b6
MWrab <mike.wohlrab@live.de>
2022-05-06 13:05:26 +0200

    test: LoginServiceTest

---
+++ src/test/java/de/uniks/pioneers/service/LoginServiceTest.java
@@ -0,0 +1,41 @@
+package de.uniks.pioneers.service;
+
+import de.uniks.pioneers.dto.LoginDto;
+import de.uniks.pioneers.dto.LoginResult;
+import de.uniks.pioneers.rest.AuthApiService;
+import io.reactivex.rxjava3.core.Observable;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class LoginServiceTest {
+    @Spy
+    TokenStorage tokenStorage;
+
+    @Mock
+    AuthApiService authApiService;
+
+    @InjectMocks
+    LoginService loginService;
+
+    @Test
+    void login() {
+        when(authApiService.login(any())).thenReturn(Observable.just(new LoginResult("123", "name", "status", "avatar", "accessToken", "refreshToken")));
+
+        final String result = loginService.login("username", "password").blockingFirst();
+        assertEquals("name", result);
+
+        assertEquals("accessToken", tokenStorage.getToken());
+
+        verify(authApiService).login(new LoginDto("username", "password"));
+    }
+}
