616f9232e489df3991d1b0ae8875a6716ec6443e
Vadim M <vad.marchenko@gmx.de>
2022-05-07 02:04:35 +0200

    feat: init message service

---
+++ src/main/java/de/uniks/pioneers/rest/MessageApiService.java
@@ -0,0 +1,9 @@
+package de.uniks.pioneers.rest;
+
+import de.uniks.pioneers.model.Message;
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.POST;
+
+public interface MessageApiService {
+
+}
