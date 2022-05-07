2d8423556aa254ab44c3ee5f3b364f3484cf6861
Philipp L <P.Leibl@gmx.de>
2022-05-07 23:35:13 +0200

    fix: removed AuthApiService from TestModule

--- src/test/java/de/uniks/pioneers/TestModule.java
+++ src/test/java/de/uniks/pioneers/TestModule.java
@@ -56,14 +56,4 @@ public class TestModule {
 			}
 		};
 	}
-
-	@Provides
-	static AuthApiService authApiService(){
-		return new AuthApiService() {
-			@Override
-			public Observable<LoginResult> login(LoginDto dto) {
-				return Observable.empty();
-			}
-		};
-	}
 }
