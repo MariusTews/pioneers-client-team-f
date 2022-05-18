12a1ce2f2d0a04c195b065e1f40364c9771cd475
Vadim M <vad.marchenko@gmx.de>
2022-05-18 17:58:43 +0200

    test: GameLobbyControllerTest

--- src/test/java/de/uniks/pioneers/controller/GameLobbyControllerTest.java
+++ src/test/java/de/uniks/pioneers/controller/GameLobbyControllerTest.java
@@ -1,20 +1,29 @@
 package de.uniks.pioneers.controller;
 
 import de.uniks.pioneers.App;
-import de.uniks.pioneers.service.GameService;
-import de.uniks.pioneers.service.MemberService;
-import de.uniks.pioneers.service.MessageService;
-import de.uniks.pioneers.service.UserService;
+import de.uniks.pioneers.Websocket.EventListener;
+import de.uniks.pioneers.service.*;
+import io.reactivex.rxjava3.core.Observable;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class GameLobbyControllerTest extends ApplicationTest {
+    @Spy
+    GameIDStorage gameIDStorage;
+
+    @Spy
+    MemberIDStorage memberIDStorage;
+
     @Mock
     MemberService memberService;
 
@@ -27,12 +36,19 @@ public class GameLobbyControllerTest extends ApplicationTest {
     @Mock
     GameService gameService;
 
+    @Mock
+    EventListener eventListener;
+
     @InjectMocks
     GameLobbyController gameLobbyController;
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        // init calls
+        when(memberService.getAllGameMembers(any())).thenReturn(Observable.empty());
+        when(userService.findAllUsers()).thenReturn(Observable.empty());
+        when(messageService.getAllMessages(any(), any())).thenReturn(Observable.empty());
+        when(eventListener.listen(any(), any())).thenReturn(Observable.empty());
         new App(gameLobbyController).start(stage);
     }
 
