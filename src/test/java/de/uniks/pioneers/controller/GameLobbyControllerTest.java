2a7fc210d9db8620823a21f3176ffc6dce30be29
Vadim M <vad.marchenko@gmx.de>
2022-05-20 03:26:00 +0200

    test: render initial messages

--- src/test/java/de/uniks/pioneers/controller/GameLobbyControllerTest.java
+++ src/test/java/de/uniks/pioneers/controller/GameLobbyControllerTest.java
@@ -9,7 +9,6 @@ import de.uniks.pioneers.model.User;
 import de.uniks.pioneers.service.*;
 import io.reactivex.rxjava3.core.Observable;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -20,21 +19,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.matcher.control.TextInputControlMatchers;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class GameLobbyControllerTest extends ApplicationTest {
     @Spy
     GameIDStorage gameIDStorage;
-    @Spy
-    MemberIDStorage memberIDStorage;
-
     @Mock
     MemberService memberService;
     @Mock
@@ -65,14 +58,12 @@ public class GameLobbyControllerTest extends ApplicationTest {
 
         Game g = new Game("0", "0", "g1", "g", "u1",  2);
 
-        this.gameIDStorage.setId("g1");
-
+        when(gameIDStorage.getId()).thenReturn("g1");
         when(memberService.getAllGameMembers(any())).thenReturn(Observable.just(m1, m2).buffer(2));
         when(userService.findAllUsers()).thenReturn(Observable.just(u1, u2).buffer(2));
         when(userService.findOne(any())).thenReturn(Observable.empty());
         when(eventListener.listen(any(), any())).thenReturn(Observable.empty());
         when(gameService.findOneGame(any())).thenReturn(Observable.just(g));
-        when(messageService.send(any(), any(), any())).thenReturn(Observable.just(x1));
         when(messageService.getAllMessages(any(), any())).thenReturn(Observable.just(x1, x2).buffer(2));
 
         this.stage = stage;
@@ -81,25 +72,13 @@ public class GameLobbyControllerTest extends ApplicationTest {
     }
 
     @Test
-    public void sendMessageTest() {
-        // jump to messageField
-        write("\ttest1");
-        FxAssert.verifyThat("#idMessageField", TextInputControlMatchers.hasText("test1"));
-
-        write("\t");
-        type(KeyCode.SPACE);
-
+    public void renderMessageTest() {
         VBox box = lookup("#idMessageView").query();
         HBox messages = (HBox) box.getChildren().get(0);
         Label label = (Label) messages.getChildren().get(1);
 
         Assertions.assertEquals("a: test1", label.getText());
 
-        verify(messageService).send("games", "g1", "test1");
-        verify(memberService).getAllGameMembers("g1");
-        verify(userService).findAllUsers();
-        verify(userService).findOne("u1");
-        verify(eventListener).listen("games.g1.messages.*.*", Message.class);
         verify(messageService).getAllMessages("games", "g1");
     }
 }
