2617b16674a063256e42a4a3e4749aec175bbd33
MWrab <mike.wohlrab@live.de>
2022-05-26 23:46:01 +0200

    feat: implement gameFieldView in GameScreenController

--- src/main/java/de/uniks/pioneers/controller/GameScreenController.java
+++ src/main/java/de/uniks/pioneers/controller/GameScreenController.java
@@ -1,16 +1,27 @@
 package de.uniks.pioneers.controller;
 
+import de.uniks.pioneers.App;
 import de.uniks.pioneers.Main;
+import de.uniks.pioneers.model.Game;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
 
 import javax.inject.Inject;
 import java.io.IOException;
 
 public class GameScreenController implements Controller {
 
+    private GameFieldSubController gameFieldSubController;
+
+    @FXML
+    public Pane mapPane;
+
+
     @Inject
-    public GameScreenController(){}
+    public GameScreenController(){
+    }
 
 
     @Override
@@ -34,6 +45,9 @@ public class GameScreenController implements Controller {
             e.printStackTrace();
             return null;
         }
+
+        this.gameFieldSubController = new GameFieldSubController();
+        mapPane.getChildren().setAll(gameFieldSubController.render());
 
         return parent;
     }
