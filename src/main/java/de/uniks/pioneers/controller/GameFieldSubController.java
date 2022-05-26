09d94b2630c2eda80c04546c23b5edda8eec7e7e
MWrab <mike.wohlrab@live.de>
2022-05-26 23:44:13 +0200

    feat: GameFieldSubController

---
+++ src/main/java/de/uniks/pioneers/controller/GameFieldSubController.java
@@ -0,0 +1,46 @@
+package de.uniks.pioneers.controller;
+
+import de.uniks.pioneers.App;
+import de.uniks.pioneers.Main;
+import de.uniks.pioneers.model.Game;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+public class GameFieldSubController implements Controller{
+
+    private Parent parent;
+
+    @Inject
+    public GameFieldSubController(){
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public Parent render() {
+        final FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/GameFieldSubView.fxml"));
+        loader.setControllerFactory(c -> this);
+        final Parent parent;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        
+
+        this.parent = parent;
+        return parent;
+    }
+}
