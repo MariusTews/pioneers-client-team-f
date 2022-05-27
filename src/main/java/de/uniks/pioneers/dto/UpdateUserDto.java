e5e7a4aa129bcd03e05b84705ecd58f03548f1ff
jessibld <jessibaldin@gmail.com>
2022-05-27 22:21:38 +0200

    feat: add friends to User, UserService and UpdateUserDto

--- src/main/java/de/uniks/pioneers/dto/UpdateUserDto.java
+++ src/main/java/de/uniks/pioneers/dto/UpdateUserDto.java
@@ -1,10 +1,11 @@
 package de.uniks.pioneers.dto;
 
-import java.util.Optional;
+import java.util.List;
 
 public record UpdateUserDto(
 		String name,
 		String status,
 		String avatar,
+		List<String> friends,
 		String password
 ) {}
--- src/main/java/de/uniks/pioneers/model/User.java
+++ src/main/java/de/uniks/pioneers/model/User.java
@@ -1,8 +1,11 @@
 package de.uniks.pioneers.model;
 
+import java.util.List;
+
 public record User(
 		String _id,
 		String name,
 		String status,
-		String avatar
+		String avatar,
+		List<String> friends
 ) {}
