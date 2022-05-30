aa5733165fceb35952e0305ba5654f6a72f9d9d7
MWrab <mike.wohlrab@live.de>
2022-05-30 23:03:26 +0200

    feat: GameScreenController

--- src/main/java/de/uniks/pioneers/controller/GameScreenController.java
+++ src/main/java/de/uniks/pioneers/controller/GameScreenController.java
@@ -2,32 +2,41 @@ package de.uniks.pioneers.controller;
 
 import de.uniks.pioneers.App;
 import de.uniks.pioneers.Main;
-import de.uniks.pioneers.model.Game;
+
+import de.uniks.pioneers.service.GameIDStorage;
+import de.uniks.pioneers.service.PioneersService;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.Pane;
 
 import javax.inject.Inject;
-import javax.inject.Provider;
+
 import java.io.IOException;
+
 
 public class GameScreenController implements Controller {
 
-    private GameFieldSubController gameFieldSubController;
+
+
 
     @FXML
     public Pane mapPane;
     private App app;
-    private Game game;
-    private Provider<GameLobbyController> gameLobbyController;
+    private GameIDStorage gameIDStorage;
+    private PioneersService pioneersService;
+
+    private GameFieldSubController gameFieldSubController;
 
 
     @Inject
-    public GameScreenController(App app, Game game, Provider<GameLobbyController> gameLobbyController){
+    public GameScreenController(App app,
+                                GameIDStorage gameIDStorage,
+                                PioneersService pioneersService){
         this.app = app;
-        this.game = game;
-        this.gameLobbyController = gameLobbyController;
+        this.gameIDStorage = gameIDStorage;
+        this.pioneersService = pioneersService;
     }
 
 
@@ -35,6 +44,9 @@ public class GameScreenController implements Controller {
     public void init() {
 
     }
+
+
+
 
     @Override
     public void destroy() {
@@ -53,9 +65,11 @@ public class GameScreenController implements Controller {
             return null;
         }
 
-        this.gameFieldSubController = new GameFieldSubController(app, game);
+        this.gameFieldSubController = new GameFieldSubController(app, gameIDStorage, pioneersService);
         mapPane.getChildren().setAll(gameFieldSubController.render());
 
         return parent;
     }
+
+
 }
