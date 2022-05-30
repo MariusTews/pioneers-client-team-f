55f3c6365f59c22290dbb817d28199988b53e114
Deadbymidnight <i.psye@gmail.com>
2022-05-30 23:44:11 +0200

    added subcontroller

---
+++ src/main/java/de/uniks/pioneers/controller/userSubView.java
@@ -0,0 +1,20 @@
+package de.uniks.pioneers.controller;
+
+import javafx.scene.Parent;
+
+public class userSubView implements Controller {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
