0d0a4582e37d3785fdcb7d92bd697f5909e44033
r-heisler <robin-heisler@gmx.de>
2022-06-01 20:39:50 +0200

    feat foundingRoll

--- src/main/java/de/uniks/pioneers/controller/GameScreenController.java
+++ src/main/java/de/uniks/pioneers/controller/GameScreenController.java
@@ -3,17 +3,27 @@ package de.uniks.pioneers.controller;
 import de.uniks.pioneers.App;
 import de.uniks.pioneers.Main;
 
+import de.uniks.pioneers.Websocket.EventListener;
+import de.uniks.pioneers.dto.CreateMoveDto;
+import de.uniks.pioneers.model.Message;
 import de.uniks.pioneers.service.GameIDStorage;
 import de.uniks.pioneers.service.PioneersService;
 
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 import javax.inject.Inject;
 
 import java.io.IOException;
+
+import static de.uniks.pioneers.Constants.CREATED;
+import static de.uniks.pioneers.Constants.FX_SCHEDULER;
 
 
 public class GameScreenController implements Controller {
@@ -23,25 +33,38 @@ public class GameScreenController implements Controller {
 
     @FXML
     public Pane mapPane;
+    public Button rollButton;
+    public Label diceLabel;
     private App app;
     private GameIDStorage gameIDStorage;
     private PioneersService pioneersService;
 
     private GameFieldSubController gameFieldSubController;
+    private final CompositeDisposable disposable = new CompositeDisposable();
+    private final EventListener eventListener;
 
 
     @Inject
     public GameScreenController(App app,
                                 GameIDStorage gameIDStorage,
-                                PioneersService pioneersService){
+                                PioneersService pioneersService,
+                                EventListener eventListener){
         this.app = app;
         this.gameIDStorage = gameIDStorage;
         this.pioneersService = pioneersService;
+        this.eventListener = eventListener;
     }
 
 
     @Override
     public void init() {
+        disposable.add(eventListener
+                .listen("games." + this.gameIDStorage.getId() + ".*.*", Message.class)
+                .observeOn(FX_SCHEDULER)
+                .subscribe(event->{
+                    final Message message = event.data();
+                    System.out.println(event.event());
+                }));
 
     }
 
@@ -67,9 +90,15 @@ public class GameScreenController implements Controller {
 
         this.gameFieldSubController = new GameFieldSubController(app, gameIDStorage, pioneersService);
         mapPane.getChildren().setAll(gameFieldSubController.render());
+        System.out.println(gameIDStorage.getId());
 
         return parent;
     }
 
 
+    public void rollDice(ActionEvent event) {
+        pioneersService.move(gameIDStorage.getId(),"founding-roll",0,0,0,0,"settlement")
+                .observeOn(FX_SCHEDULER)
+                .subscribe(roll-> this.diceLabel.setText("" + roll.roll()));
+    }
 }
