4cc7a97bceb97dcd06829b15dc8236faf5109dbe
jessibld <jessibaldin@gmail.com>
2022-06-06 18:19:34 +0200

    feat: add OpponentSubController, fxml for opponents, load and refresh opponent view of game screen

--- src/main/java/de/uniks/pioneers/controller/GameScreenController.java
+++ src/main/java/de/uniks/pioneers/controller/GameScreenController.java
@@ -4,26 +4,33 @@ import de.uniks.pioneers.App;
 import de.uniks.pioneers.Main;
 
 import de.uniks.pioneers.Websocket.EventListener;
-import de.uniks.pioneers.model.ExpectedMove;
-import de.uniks.pioneers.model.Move;
-import de.uniks.pioneers.model.State;
+import de.uniks.pioneers.dto.Event;
+import de.uniks.pioneers.model.*;
 import de.uniks.pioneers.service.*;
 
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import static de.uniks.pioneers.Constants.FX_SCHEDULER;
+import static de.uniks.pioneers.Constants.*;
 
 public class GameScreenController implements Controller {
+
+    private final ObservableList<Member> members = FXCollections.observableArrayList();
 
     @FXML
     public Pane mapPane;
@@ -31,6 +38,8 @@ public class GameScreenController implements Controller {
     public Pane chatPane;
     @FXML
     public Label diceSumLabel;
+    @FXML
+    public VBox opponentsView;
 
     private final App app;
     private final GameIDStorage gameIDStorage;
@@ -40,10 +49,11 @@ public class GameScreenController implements Controller {
     private final UserService userService;
     private final MessageService messageService;
     private final MemberService memberService;
-
-    private GameFieldSubController gameFieldSubController;
-    private MessageViewSubController messageViewSubController;
     private final CompositeDisposable disposable = new CompositeDisposable();
+    private final List<OpponentSubController> opponentSubCons = new ArrayList<>();
+    private final HashMap<String, User> userHash = new HashMap<>();
+
+    private MessageViewSubController messageViewSubController;
 
 
     @Inject
@@ -68,6 +78,25 @@ public class GameScreenController implements Controller {
     @Override
     public void init() {
 
+        // For later : userHash is needed in MessageViewSubController too,
+        // improvement would be to not initialize the hash twice.
+        // Get all users for the username and avatar. Save in HashMap to find the user by his/her ID
+        userService
+                .findAllUsers()
+                .observeOn(FX_SCHEDULER)
+                .subscribe(result -> {
+                    for (User user : result) {
+                        this.userHash.put(user._id(), user);
+                    }
+                });
+
+        // Get all members of the game for loading the opponents
+        // ATTENTION: it is not possible to pass the observable list of members via constructor!
+        memberService
+                .getAllGameMembers(this.gameIDStorage.getId())
+                .observeOn(FX_SCHEDULER)
+                .subscribe(this.members::setAll);
+
         disposable.add(eventListener
                 .listen("games." + this.gameIDStorage.getId() + ".moves.*." + "created", Move.class)
                 .observeOn(FX_SCHEDULER)
@@ -78,11 +107,19 @@ public class GameScreenController implements Controller {
                     }
                 }));
 
+        // Listen to the members to get to know if a member of the game leaves or joins the game
+        disposable.add(eventListener
+                .listen("games." + this.gameIDStorage.getId() + ".members.*.*", Member.class)
+                .observeOn(FX_SCHEDULER)
+                .subscribe(this::handleMemberEvents));
 
         // Initialize sub controller for ingame chat, add listener and load all messages
         this.messageViewSubController = new MessageViewSubController(eventListener, gameIDStorage,
                 userService, messageService, memberIDStorage, memberService);
         messageViewSubController.init();
+
+        // Load all opponents
+        this.initAllOpponents();
     }
 
     @Override
@@ -91,6 +128,9 @@ public class GameScreenController implements Controller {
             this.messageViewSubController.destroy();
         }
         disposable.dispose();
+
+        this.opponentSubCons.forEach(OpponentSubController::destroy);
+        this.opponentSubCons.clear();
     }
 
     @Override
@@ -105,13 +145,59 @@ public class GameScreenController implements Controller {
             return null;
         }
 
-        this.gameFieldSubController = new GameFieldSubController(app, gameIDStorage, pioneersService);
+        GameFieldSubController gameFieldSubController = new GameFieldSubController(app, gameIDStorage, pioneersService);
         mapPane.getChildren().setAll(gameFieldSubController.render());
 
         // Show chat and load the messages
         chatPane.getChildren().setAll(messageViewSubController.render());
 
         return parent;
+    }
+
+    private void handleMemberEvents(Event<Member> memberEvent) {
+        // Handle event on player and refresh list of players
+        Member member = memberEvent.data();
+
+        if (memberEvent.event().endsWith(CREATED)) {
+            this.members.add(member);
+            this.opponentsView.getChildren().add(renderOpponent(member));
+        } else if (memberEvent.event().endsWith(DELETED)) {
+            this.members.removeIf(p -> p.userId().equals(member.userId()));
+            // Remove opponent sub-controller and render the opponent list again without the game member
+            this.removeOpponent(member);
+        }
+    }
+
+    private void removeOpponent(Member member) {
+        // Remove sub-controller of opponent who left
+        for (OpponentSubController subCon : this.opponentSubCons) {
+            if (subCon.getId().equals(member.userId())) {
+                this.opponentSubCons.remove(subCon);
+                break;
+            }
+        }
+        this.initAllOpponents();
+    }
+
+    // Load the opponent view with username and avatar
+    private Node renderOpponent(Member member) {
+        // user's view loads the opponents without the user himself/herself,
+        // because user has own view with stats
+        if (!member.userId().equals(memberIDStorage.getId())) {
+            // User as parameter for getting avatar and username
+            OpponentSubController opponentCon = new OpponentSubController(member, this.userHash.get(member.userId()));
+            opponentSubCons.add(opponentCon);
+            return opponentCon.render();
+        }
+        return null;
+    }
+
+    private void initAllOpponents() {
+        // If an opponent left, the whole view has to be loaded again
+        this.opponentsView.getChildren().clear();
+        for (Member member : this.members) {
+          this.opponentsView.getChildren().add(this.renderOpponent(member));
+        }
     }
 
     public void onMouseClicked(MouseEvent mouseEvent) {
