d4f1e525a7dc6b80d8026e5457757175f7c4dc60
jessibld <jessibaldin@gmail.com>
2022-06-10 15:03:39 +0200

    feat: add images for resources

--- src/main/java/de/uniks/pioneers/controller/OpponentSubController.java
+++ src/main/java/de/uniks/pioneers/controller/OpponentSubController.java
@@ -1,12 +1,13 @@
 package de.uniks.pioneers.controller;
 
 import de.uniks.pioneers.Main;
-import de.uniks.pioneers.model.Member;
+import de.uniks.pioneers.model.Player;
 import de.uniks.pioneers.model.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -24,30 +25,40 @@ public class OpponentSubController implements Controller {
     ImageView avatarImageView;
     @FXML
     Label usernameLabel;
-    // TODO: in the second release victory points are not given, have to calculate them
     @FXML
     Label victoryPointsLabel;
-    // TODO: Amount of opponent's resources is not displayed, add mouse hover for resource names
-    //  and add images of resources
     @FXML
     HBox resourcesView;
-    private final Member opponent;
+    @FXML
+    ImageView earthCactusImage;
+    @FXML
+    ImageView marsBarImage;
+    @FXML
+    ImageView moonRockImage;
+    @FXML
+    ImageView neptunCrystalsImage;
+    @FXML
+    ImageView venusGrainImage;
+
+    private final Player opponent;
     private final User opponentAsUser;
+    private final int victoryPoints;
     private String userId;
     private Parent parent;
 
     @Inject
-    public OpponentSubController(Member member, User user) {
+    public OpponentSubController(Player player, User user, int victoryPoints) {
         // Opponent as player needed for victory points and color
-        this.opponent = member;
+        this.opponent = player;
 
         // Opponents as user needed for username and avatar
         this.opponentAsUser = user;
+
+        this.victoryPoints = victoryPoints;
     }
 
     @Override
     public void init() {
-
     }
 
     @Override
@@ -69,13 +80,23 @@ public class OpponentSubController implements Controller {
 
         if (this.opponent != null) {
             this.userId = this.opponent.userId();
-            this.usernameLabel.setText(this.opponentAsUser.name());
+            if (this.opponentAsUser == null) {
+                this.usernameLabel.setText("UNKNOWN");
+            } else {
+                this.usernameLabel.setText(this.opponentAsUser.name());
+            }
             this.usernameLabel.setTextFill(Color.web(this.opponent.color()));
-            if (this.opponentAsUser.avatar() != null) {
+            if (this.opponentAsUser != null && this.opponentAsUser.avatar() != null) {
                 this.avatarImageView.setImage(new Image(this.opponentAsUser.avatar()));
             }
-            this.victoryPointsLabel.setText("UP: " + " /10");
+            this.victoryPointsLabel.setText("VP: " + victoryPoints + "/10");
         }
+
+        Tooltip.install(earthCactusImage, new Tooltip("Earth cactus"));
+        Tooltip.install(marsBarImage, new Tooltip("Mars bar"));
+        Tooltip.install(moonRockImage, new Tooltip("Moon rock"));
+        Tooltip.install(neptunCrystalsImage, new Tooltip("Neptun crystals"));
+        Tooltip.install(venusGrainImage, new Tooltip("Venus grain"));
 
         this.parent = parent;
         return parent;
