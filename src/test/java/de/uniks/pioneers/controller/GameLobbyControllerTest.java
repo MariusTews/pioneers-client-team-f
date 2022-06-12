4fed423a150e94ab97d8b78da77e354a6151e639
Deadbymidnight <i.psye@gmail.com>
2022-06-12 19:07:39 +0200

    added required changes

--- src/test/java/de/uniks/pioneers/controller/GameLobbyControllerTest.java
+++ src/test/java/de/uniks/pioneers/controller/GameLobbyControllerTest.java
@@ -1,14 +1,32 @@
 package de.uniks.pioneers.controller;
 
+import de.uniks.pioneers.App;
+import de.uniks.pioneers.Websocket.EventListener;
+import de.uniks.pioneers.model.Game;
+import de.uniks.pioneers.model.Member;
+import de.uniks.pioneers.service.*;
+import io.reactivex.rxjava3.core.Observable;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.framework.junit5.ApplicationTest;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GameLobbyControllerTest extends ApplicationTest {
     // TODO: fix test
 
-    /*@Mock
+    @Mock
     UserService userService;
 
     @Mock
@@ -43,13 +61,11 @@ class GameLobbyControllerTest extends ApplicationTest {
     public void start(Stage stage) {
 
         when(userService.findAllUsers()).thenReturn(Observable.empty());
-        when(userService.findOne(any())).thenReturn(Observable.empty());
-        when(eventListener.listen(any(), any())).thenReturn(Observable.empty());
-        when(memberService.getAllGameMembers("id")).thenReturn(Observable.just(List.of(new Member("0:00",
+        when(memberService.getAllGameMembers(any())).thenReturn(Observable.just(List.of(new Member("0:00",
                 "0:30", "id", "4", false, "#000000"))));
-        when(gameService.findOneGame(any())).thenReturn(Observable.just(new Game("0:00", "0:30",
+        when(eventListener.listen(any(), any())).thenReturn(Observable.empty());
+        when(gameService.findOneGame("id")).thenReturn(Observable.just(new Game("0:00", "0:30",
                                                         "id", "name", "4", 2, false)));
-        when(gameIDStorage.getId()).thenReturn("id");
 
 
         // start application
@@ -62,23 +78,21 @@ class GameLobbyControllerTest extends ApplicationTest {
     @Test
     void leave() {
 
-        when(idStorage.getID()).thenReturn("4");
+        when(idStorage.getID()).thenReturn("2");
         when(gameIDStorage.getId()).thenReturn("id");
-        when(memberService.leave("id", "4")).thenReturn(Observable.just(new Member("0:00",
-                "0:30", "id", "4", false, "#000000")));
+        when(memberService.leave("id", "2")).thenReturn(Observable.just(new Member("0:00",
+                "0:30", "id", "2", false, "#000000")));
 
         write("\t\t\t\t");
         type(KeyCode.SPACE);
 
-        verify(memberService).leave("id", "4");
+        verify(memberService).leave("id", "2");
     }
 
     @Test
     void leaveLastMember() {
         when(idStorage.getID()).thenReturn("4");
         when(gameIDStorage.getId()).thenReturn("id");
-        when(gameService.findOneGame("id")).thenReturn(Observable.just(new Game("0:00", "0:30",
-                                                    "id", "name", "owner", 1, false)));
         when(gameService.deleteGame("id")).thenReturn(Observable.just(new Game("0:00", "0:30",
                                                         "id", "name", "owner", 1, false)));
 
@@ -88,5 +102,5 @@ class GameLobbyControllerTest extends ApplicationTest {
         verify(gameService).deleteGame("id");
 
 
-    }*/
+    }
 }
