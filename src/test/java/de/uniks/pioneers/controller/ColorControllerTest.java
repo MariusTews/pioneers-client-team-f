332e357b9f33d25f672332e2a213e93a5bc74ce5
Deadbymidnight <i.psye@gmail.com>
2022-06-17 04:29:08 +0200

    changes

---
+++ src/test/java/de/uniks/pioneers/controller/ColorControllerTest.java
@@ -0,0 +1,46 @@
+package de.uniks.pioneers.controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ColorControllerTest extends ApplicationTest {
+
+
+    ColorController colorcontroller;
+
+    @BeforeEach
+    public void setup(){
+        colorcontroller = mock(ColorController.class);
+    }
+
+    @Test
+    public void coloTest(){
+        Label a1 = new Label();
+        a1.setText("GREEN");
+        a1.setTextFill(Color.GREEN);
+        a1.setMinWidth(120);
+        a1.setStyle("-fx-border-color: blue");
+        List<Label> listLabel = new ArrayList<>();
+        listLabel.add(a1);
+        when(colorcontroller.getColor()).thenReturn((listLabel));
+        colorcontroller.getColor();
+
+        verify(colorcontroller).getColor();
+
+        Assertions.assertEquals(listLabel,colorcontroller.getColor());
+    }
+
+
+}
