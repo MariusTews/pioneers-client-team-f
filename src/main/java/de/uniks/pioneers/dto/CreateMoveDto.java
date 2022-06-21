5a284215a15e443380ebd3c6ed628faf708428ae
Philipp L <P.Leibl@gmx.de>
2022-06-21 23:42:29 +0200

    feat: changed Dto to Server v3

--- src/main/java/de/uniks/pioneers/dto/CreateMoveDto.java
+++ src/main/java/de/uniks/pioneers/dto/CreateMoveDto.java
@@ -1,6 +1,11 @@
 package de.uniks.pioneers.dto;
 
+import java.util.HashMap;
+
 public record CreateMoveDto(
         String action,
+		RobDto rob,
+		HashMap<String, Integer> resources,
+		String partner,
         CreateBuildingDto building
 ) {}
