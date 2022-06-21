89343b6024515acc8fdb0bed26c7562704ea4f92
Philipp L <P.Leibl@gmx.de>
2022-06-22 00:51:59 +0200

    feat: adapt PioneersApiService

--- src/main/java/de/uniks/pioneers/rest/PioneersApiService.java
+++ src/main/java/de/uniks/pioneers/rest/PioneersApiService.java
@@ -9,27 +9,36 @@ import java.util.List;
 
 public interface PioneersApiService {
 
-    @GET("games/{gameId}/map")
-    Observable<Map> findAllTiles(@Path("gameId") String gameId);
+	@GET("games/{gameId}/map")
+	Observable<Map> findAllTiles(@Path("gameId") String gameId);
 
-    @GET("games/{gameId}/players")
-    Observable<List<Player>> findAllPlayers(@Path("gameId") String gameId);
+	@GET("games/{gameId}/players")
+	Observable<List<Player>> findAllPlayers(@Path("gameId") String gameId);
 
-    @GET("games/{gameId}/players/{userId}")
-    Observable<Player> findOnePlayer(@Path("gameId") String gameId,
-                                           @Path("userId") String userId);
+	@GET("games/{gameId}/players/{userId}")
+	Observable<Player> findOnePlayer(@Path("gameId") String gameId,
+									 @Path("userId") String userId);
 
-    @GET("games/{gameId}/state")
-    Observable<State> findOneState(@Path("gameId") String gameId);
+	@PATCH("games/{gameId}/players/{userId}")
+	Observable<Player> updatePlayer(@Path("gameId") String gameId, @Path("userId") String userId);
 
-    @GET("games/{gameId}/buildings")
-    Observable<List<Building>> findAllBuildings(@Path("gameId") String gameId);
+	@GET("games/{gameId}/state")
+	Observable<State> findOneState(@Path("gameId") String gameId);
 
-    @GET("games/{gameId}/buildings/{buildingId}")
-    Observable<Building> findOneBuilding(@Path("gameId") String gameId,
-                                         @Path("buildingId") String buildingId);
+	@GET("games/{gameId}/buildings")
+	Observable<List<Building>> findAllBuildings(@Path("gameId") String gameId);
 
-    @POST("games/{gameId}/moves")
-    Observable<Move> create(@Path("gameId") String gameId,
-                            @Body CreateMoveDto dto);
+	@GET("games/{gameId}/buildings/{buildingId}")
+	Observable<Building> findOneBuilding(@Path("gameId") String gameId,
+										 @Path("buildingId") String buildingId);
+
+	@POST("games/{gameId}/moves")
+	Observable<Move> create(@Path("gameId") String gameId,
+							@Body CreateMoveDto dto);
+
+	@GET("games/{gameId}/moves")
+	Observable<List<Move>> findAllMoves(@Path("gameId") String gameId);
+
+	@GET("games/{gameId}/moves/{moveId}")
+	Observable<Move> findOneMove(@Path("gameId") String gameId, @Path("moveId") String moveId);
 }
