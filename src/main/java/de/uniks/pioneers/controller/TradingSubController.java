7339c1afe36001e614e6c428602eb624a3df25fa
Vadim M <vad.marchenko@gmx.de>
2022-06-26 22:38:55 +0200

    add: TradingSubView and TradingSubController

---
+++ src/main/java/de/uniks/pioneers/controller/TradingSubController.java
@@ -0,0 +1,164 @@
+package de.uniks.pioneers.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import javax.inject.Inject;
+
+public class TradingSubController implements Controller{
+    @FXML
+    public Button giveCactusMinusButton;
+    @FXML
+    public Label giveCactusLabel;
+    @FXML
+    public Button giveCactusPlusButton;
+    @FXML
+    public Button giveMarsMinusButton;
+    @FXML
+    public Label giveMarsLabel;
+    @FXML
+    public Button giveMarsPlusButton;
+    @FXML
+    public Button giveMoonMinusButton;
+    @FXML
+    public Label giveMoonLabel;
+    @FXML
+    public Button giveMoonPlusButton;
+    @FXML
+    public Button giveNeptunMinusButton;
+    @FXML
+    public Label giveNeptunLabel;
+    @FXML
+    public Button giveNeptunPlusButton;
+    @FXML
+    public Button giveVenusMinusButton;
+    @FXML
+    public Label giveVenusLabel;
+    @FXML
+    public Button giveVenusPlusButton;
+    @FXML
+    public Button receiveCactusMinusButton;
+    @FXML
+    public Label receiveCactusLabel;
+    @FXML
+    public Button receiveCactusPlusButton;
+    @FXML
+    public Button receiveMarsMinusButton;
+    @FXML
+    public Label receiveMarsLabel;
+    @FXML
+    public Button receiveMarsPlusButton;
+    @FXML
+    public Button receiveMoonMinusButton;
+    @FXML
+    public Label receiveMoonLabel;
+    @FXML
+    public Button receiveMoonPlusButton;
+    @FXML
+    public Button receiveNeptunMinusButton;
+    @FXML
+    public Label receiveNeptunLabel;
+    @FXML
+    public Button receiveNeptunPlusButton;
+    @FXML
+    public Button receiveVenusMinusButton;
+    @FXML
+    public Label receiveVenusLabel;
+    @FXML
+    public Button receiveVenusPlusButton;
+    @FXML
+    public Button offerPlayerButton;
+    @FXML
+    public Button offerBankButton;
+
+    private Parent parent;
+
+    @Inject
+    public TradingSubController() {
+
+    }
+
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+
+    public void giveCactusMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveCactusPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveMarsMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveMarsPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveMoonMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveMoonPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveNeptunMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveNeptunPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveVenusMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void giveVenusPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveCactusMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveCactusPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveMarsMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveMarsPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveMoonMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveMoonPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveNeptunMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveNeptunPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveVenusMinusButtonPressed(ActionEvent event) {
+    }
+
+    public void receiveVenusPlusButtonPressed(ActionEvent event) {
+    }
+
+    public void offerPlayerButtonPressed(ActionEvent event) {
+    }
+
+    public void offerBankButtonPressed(ActionEvent event) {
+    }
+}
