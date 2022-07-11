9c47af75dcbc510118187a150dd7cffa4d4d45b7
Vadim M <vad.marchenko@gmx.de>
2022-07-11 15:28:02 +0200

    fix: removed redundant imports

--- src/main/java/de/uniks/pioneers/controller/TradingSubController.java
+++ src/main/java/de/uniks/pioneers/controller/TradingSubController.java
@@ -14,14 +14,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import javax.inject.Inject;
 import java.io.IOException;
