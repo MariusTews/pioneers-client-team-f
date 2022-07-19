7745bdade7da63e43621e737a4e826c4fea294b1
mariustews <mariust2001@gmail.com>
2022-07-19 22:09:22 +0200

    feat: mapTemplateLabel and mapsButton in createGameScreen

--- src/main/java/de/uniks/pioneers/controller/CreateGameController.java
+++ src/main/java/de/uniks/pioneers/controller/CreateGameController.java
@@ -17,168 +17,181 @@ import static de.uniks.pioneers.Constants.FX_SCHEDULER;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class CreateGameController implements Controller {
-	private final Provider<GameLobbyController> gameLobbyController;
-	private final Provider<LobbyController> lobbyController;
-	private final GameService gameService;
+    private final Provider<GameLobbyController> gameLobbyController;
+    private final Provider<LobbyController> lobbyController;
+    private final GameService gameService;
 
+	private String mapTemplate;
 	private int mapSize;
-
 	private int victoryPoints;
-	@FXML
-	public TextField gameNameTextField;
 
 	@FXML
-	public Button backToLobbyButton;
+    public TextField gameNameTextField;
 	@FXML
-	public Button createGameButton;
+    public Button backToLobbyButton;
 	@FXML
-	public PasswordField passwordTextField;
+    public Button createGameButton;
 	@FXML
-	public Button mapSizeMinusButton;
+    public PasswordField passwordTextField;
 	@FXML
-	public Label mapSizeLabel;
+    public Button mapSizeMinusButton;
 	@FXML
-	public Button mapSizePlusButton;
+    public Button mapSizePlusButton;
 	@FXML
-	public Button victoryPointsMinusButton;
+    public Button victoryPointsMinusButton;
 	@FXML
-	public Label victoryPointsLabel;
+    public Button victoryPointsPlusButton;
 	@FXML
-	public Button victoryPointsPlusButton;
+	public Button mapsButton;
+	@FXML
+    public Label mapSizeLabel;
+	@FXML
+    public Label victoryPointsLabel;
+	@FXML
+    public Label mapTemplateLabel;
+
 	private final App app;
 
 	@Inject
-	public CreateGameController(App app,
-								GameService gameService,
-								Provider<LobbyController> lobbyController,
-								Provider<GameLobbyController> gameLobbyController) {
-		this.app = app;
-		this.gameLobbyController = gameLobbyController;
-		this.lobbyController = lobbyController;
-		this.gameService = gameService;
+    public CreateGameController(App app,
+                                GameService gameService,
+                                Provider<LobbyController> lobbyController,
+                                Provider<GameLobbyController> gameLobbyController) {
+        this.app = app;
+        this.gameLobbyController = gameLobbyController;
+        this.lobbyController = lobbyController;
+        this.gameService = gameService;
+    }
+
+    @Override
+    public void init() {
+        this.mapSize = 2;
+        this.victoryPoints = 10;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public Parent render() {
+        final FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/CreateGame.fxml"));
+        loader.setControllerFactory(c -> this);
+        final Parent parent;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        mapSizeLabel.setText("" + mapSize);
+        victoryPointsLabel.setText("" + victoryPoints);
+        //TODO
+		//mapTemplateLabel.setText(mapTemplate);
+
+        return parent;
+    }
+
+    public void backToLobbyButtonPressed() {
+        final LobbyController controller = lobbyController.get();
+        this.app.show(controller);
+    }
+
+	public void mapsButtonPressed() {
+		System.out.println("maps button pressed");
 	}
 
-	@Override
-	public void init() {
-		this.mapSize = 2;
-		this.victoryPoints = 10;
-	}
+    public void createGameButtonPressed() {
+        if (gameNameTextField.getText().length() > 32 || gameNameTextField.getText().length() < 1) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "the name of the game must be \nbetween 1 and 32 characters!");
+            // set style of information
+            DialogPane dialogPane = alert.getDialogPane();
+            dialogPane.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("view/stylesheets/AlertStyle.css")).toExternalForm());
+            alert.showAndWait();
+        } else if (passwordTextField.getText().length() < 1) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "password can't be empty!");
+            // set style of information
+            DialogPane dialogPane = alert.getDialogPane();
+            dialogPane.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("view/stylesheets/AlertStyle.css")).toExternalForm());
+            alert.showAndWait();
+        } else {
+            gameService.create(gameNameTextField.getText(), passwordTextField.getText(), mapSize, victoryPoints)
+                    .observeOn(FX_SCHEDULER)
+                    .subscribe(onSuccess -> app.show(gameLobbyController.get()), onError -> {
+                    });
+        }
+    }
 
-	@Override
-	public void destroy() {
+    public void mapSizeMinusButtonPressed() {
+        //reduces mapSize by One if - Button is Pressed
+        if (mapSize > 0) {
+            this.mapSizeLabel.setText("" + (--mapSize));
+        }
+        //disables and hides - Button at Minimum
+        if (mapSize == 0) {
+            this.mapSizeMinusButton.disableProperty().set(true);
+            this.mapSizeMinusButton.setVisible(false);
+        }
+        //makes + Button visible and clickable if - Button is Pressed
+        if (this.mapSizePlusButton.disableProperty().get()) {
+            this.mapSizePlusButton.disableProperty().set(false);
+            this.mapSizePlusButton.setVisible(true);
+        }
+    }
 
-	}
+    public void mapSizePlusButtonPressed() {
+        //increases mapSize by One if + Button is Pressed
+        if (mapSize < 10) {
+            this.mapSizeLabel.setText("" + (++mapSize));
+        }
+        //disables and hides + Button at Maximum
+        if (mapSize == 10) {
+            this.mapSizePlusButton.disableProperty().set(true);
+            this.mapSizePlusButton.setVisible(false);
+        }
+        //makes - Button visible and clickable if + Button is Pressed
+        if (this.mapSizeMinusButton.disableProperty().get()) {
+            this.mapSizeMinusButton.disableProperty().set(false);
+            this.mapSizeMinusButton.setVisible(true);
+        }
+    }
 
-	@Override
-	public Parent render() {
-		final FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/CreateGame.fxml"));
-		loader.setControllerFactory(c -> this);
-		final Parent parent;
-		try {
-			parent = loader.load();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-		mapSizeLabel.setText("" + mapSize);
-		victoryPointsLabel.setText("" + victoryPoints);
+    public void victoryPointsMinusButtonPressed() {
+        //reduces victoryPoints by One if - Button is Pressed
+        if (victoryPoints > 3) {
+            this.victoryPointsLabel.setText("" + (--victoryPoints));
+        }
+        //disables and hides - Button at Minimum
+        if (victoryPoints == 3) {
+            this.victoryPointsMinusButton.disableProperty().set(true);
+            this.victoryPointsMinusButton.setVisible(false);
+        }
+        //makes + Button visible and clickable if - Button is Pressed
+        if (this.victoryPointsPlusButton.disableProperty().get()) {
+            this.victoryPointsPlusButton.disableProperty().set(false);
+            this.victoryPointsPlusButton.setVisible(true);
+        }
+    }
 
-		return parent;
-	}
+    public void victoryPointsPlusButtonPressed() {
+        //increases victoryPoints by One if + Button is Pressed
+        if (victoryPoints < 15) {
+            this.victoryPointsLabel.setText("" + (++victoryPoints));
+        }
+        //disables and hides + Button at Maximum
+        if (victoryPoints == 15) {
+            this.victoryPointsPlusButton.disableProperty().set(true);
+            this.victoryPointsPlusButton.setVisible(false);
+        }
+        //makes - Button visible and clickable if + Button is Pressed
+        if (this.victoryPointsMinusButton.disableProperty().get()) {
+            this.victoryPointsMinusButton.disableProperty().set(false);
+            this.victoryPointsMinusButton.setVisible(true);
+        }
+    }
 
-	public void backToLobbyButtonPressed() {
-		final LobbyController controller = lobbyController.get();
-		this.app.show(controller);
-	}
-
-
-	public void createGameButtonPressed() {
-
-		if (gameNameTextField.getText().length() > 32 || gameNameTextField.getText().length() < 1) {
-			Alert alert = new Alert(Alert.AlertType.INFORMATION, "the name of the game must be \nbetween 1 and 32 characters!");
-			// set style of information
-			DialogPane dialogPane = alert.getDialogPane();
-			dialogPane.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("view/stylesheets/AlertStyle.css")).toExternalForm());
-			alert.showAndWait();
-		} else if (passwordTextField.getText().length() < 1) {
-			Alert alert = new Alert(Alert.AlertType.INFORMATION, "password can't be empty!");
-			// set style of information
-			DialogPane dialogPane = alert.getDialogPane();
-			dialogPane.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("view/stylesheets/AlertStyle.css")).toExternalForm());
-			alert.showAndWait();
-		} else {
-			gameService.create(gameNameTextField.getText(), passwordTextField.getText(), mapSize, victoryPoints)
-					.observeOn(FX_SCHEDULER)
-					.subscribe(onSuccess -> app.show(gameLobbyController.get()), onError -> {
-					});
-		}
-	}
-
-	public void mapSizeMinusButtonPressed() {
-		//reduces mapSize by One if - Button is Pressed
-		if (mapSize > 0) {
-			this.mapSizeLabel.setText("" + (--mapSize));
-		}
-		//disables and hides - Button at Minimum
-		if (mapSize == 0) {
-			this.mapSizeMinusButton.disableProperty().set(true);
-			this.mapSizeMinusButton.setVisible(false);
-		}
-		//makes + Button visible and clickable if - Button is Pressed
-		if (this.mapSizePlusButton.disableProperty().get()) {
-			this.mapSizePlusButton.disableProperty().set(false);
-			this.mapSizePlusButton.setVisible(true);
-		}
-	}
-
-	public void mapSizePlusButtonPressed() {
-		//increases mapSize by One if + Button is Pressed
-		if (mapSize < 10) {
-			this.mapSizeLabel.setText("" + (++mapSize));
-		}
-		//disables and hides + Button at Maximum
-		if (mapSize == 10) {
-			this.mapSizePlusButton.disableProperty().set(true);
-			this.mapSizePlusButton.setVisible(false);
-		}
-		//makes - Button visible and clickable if + Button is Pressed
-		if (this.mapSizeMinusButton.disableProperty().get()) {
-			this.mapSizeMinusButton.disableProperty().set(false);
-			this.mapSizeMinusButton.setVisible(true);
-		}
-	}
-
-	public void victoryPointsMinusButtonPressed() {
-		//reduces victoryPoints by One if - Button is Pressed
-		if (victoryPoints > 3) {
-			this.victoryPointsLabel.setText("" + (--victoryPoints));
-		}
-		//disables and hides - Button at Minimum
-		if (victoryPoints == 3) {
-			this.victoryPointsMinusButton.disableProperty().set(true);
-			this.victoryPointsMinusButton.setVisible(false);
-		}
-		//makes + Button visible and clickable if - Button is Pressed
-		if (this.victoryPointsPlusButton.disableProperty().get()) {
-			this.victoryPointsPlusButton.disableProperty().set(false);
-			this.victoryPointsPlusButton.setVisible(true);
-		}
-	}
-
-	public void victoryPointsPlusButtonPressed() {
-		//increases victoryPoints by One if + Button is Pressed
-		if (victoryPoints < 15) {
-			this.victoryPointsLabel.setText("" + (++victoryPoints));
-		}
-		//disables and hides + Button at Maximum
-		if (victoryPoints == 15) {
-			this.victoryPointsPlusButton.disableProperty().set(true);
-			this.victoryPointsPlusButton.setVisible(false);
-		}
-		//makes - Button visible and clickable if + Button is Pressed
-		if (this.victoryPointsMinusButton.disableProperty().get()) {
-			this.victoryPointsMinusButton.disableProperty().set(false);
-			this.victoryPointsMinusButton.setVisible(true);
-		}
+	public void setMapTemplate(String mapTemplate) {
+		this.mapTemplate = mapTemplate;
 	}
 }
