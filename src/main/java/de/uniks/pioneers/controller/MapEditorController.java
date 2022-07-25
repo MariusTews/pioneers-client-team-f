e4525da3457479c9b7472e59201dd379bf758729
Vadim M <vad.marchenko@gmx.de>
2022-07-25 21:36:08 +0200

    fix: javafx version 17, formatting add: loading of clear tiles and buttons

--- src/main/java/de/uniks/pioneers/controller/MapEditorController.java
+++ src/main/java/de/uniks/pioneers/controller/MapEditorController.java
@@ -1,18 +1,25 @@
 package de.uniks.pioneers.controller;
 
 import de.uniks.pioneers.Main;
+import de.uniks.pioneers.computation.CalculateMap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MapEditorController implements Controller{
+public class MapEditorController implements Controller {
     @FXML
     public TextField nameTextField;
     @FXML
@@ -23,9 +30,13 @@ public class MapEditorController implements Controller{
     public Button mapSizePlusButton;
     @FXML
     public Button saveButton;
+    @FXML
+    public ScrollPane mapPane;
+
+    private List<Polygon> hexagons = new ArrayList<>();
 
     @Inject
-    public  MapEditorController() {
+    public MapEditorController() {
     }
 
     @Override
@@ -50,7 +61,30 @@ public class MapEditorController implements Controller{
             e.printStackTrace();
             return null;
         }
+
+        Pane map = new CalculateMap().buildMap(2, true);
+
+        this.mapPane.setContent(map);
+
+        for (Node node : map.getChildren()) {
+            if (node.getId().endsWith("_harborButton")) {
+                Button button = (Button) node;
+                button.setOnAction(this::harborButtonPressed);
+            } else if (node.getId().endsWith("_tileButton")) {
+                Button button = (Button) node;
+                button.setOnAction(this::tileButtonPressed);
+            } else {
+                this.hexagons.add((Polygon) node);
+            }
+        }
+
         return parent;
+    }
+
+    private void tileButtonPressed(ActionEvent event) {
+    }
+
+    private void harborButtonPressed(ActionEvent event) {
     }
 
     public void mapSizeMinusButtonPressed(ActionEvent event) {
