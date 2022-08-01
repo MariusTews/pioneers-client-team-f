b2c7e6473585f96501c0df23f1c5bdfde7862b96
Philipp L <P.Leibl@gmx.de>
2022-08-01 16:59:44 +0200

    feat: add startingResources and rollSeven to GameStorage

--- src/main/java/de/uniks/pioneers/service/GameStorage.java
+++ src/main/java/de/uniks/pioneers/service/GameStorage.java
@@ -5,45 +5,62 @@ import javax.inject.Singleton;
 
 @Singleton
 public class GameStorage {
-    private String id;
+	private String id;
+	private String mapTemplate;
+	private int size;
+	private int victoryPoints;
+	private boolean rollSeven;
+	private int startingResources;
 
-    private String mapTemplate;
-    private int size;
-    private int victoryPoints;
+	@Inject
+	public GameStorage() {
+	}
 
-    @Inject
-    public GameStorage() {
-    }
+	public String getId() {
+		return this.id;
+	}
 
-    public String getId() {
-        return this.id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public int getSize() {
+		return size;
+	}
 
-    public int getSize() {
-        return size;
-    }
+	public void setSize(int size) {
+		this.size = size;
+	}
 
-    public void setSize(int size) {
-        this.size = size;
-    }
+	public int getVictoryPoints() {
+		return victoryPoints;
+	}
 
-    public int getVictoryPoints() {
-        return victoryPoints;
-    }
+	public void setVictoryPoints(int victoryPoints) {
+		this.victoryPoints = victoryPoints;
+	}
 
-    public void setVictoryPoints(int victoryPoints) {
-        this.victoryPoints = victoryPoints;
-    }
+	public String getMapTemplate() {
+		return mapTemplate;
+	}
 
-    public String getMapTemplate() {
-        return mapTemplate;
-    }
+	public void setMapTemplate(String mapTemplate) {
+		this.mapTemplate = mapTemplate;
+	}
 
-    public void setMapTemplate(String mapTemplate) {
-        this.mapTemplate = mapTemplate;
-    }
+	public boolean isRollSeven() {
+		return rollSeven;
+	}
+
+	public void setRollSeven(boolean rollSeven) {
+		this.rollSeven = rollSeven;
+	}
+
+	public int getStartingResources() {
+		return startingResources;
+	}
+
+	public void setStartingResources(int startingResources) {
+		this.startingResources = startingResources;
+	}
 }
