e63ab7b4098b078d9c9bb7c43082796271c97ee4
Philipp L <P.Leibl@gmx.de>
2022-08-08 05:22:45 +0200

    feat: added MapTemplateViewController

---
+++ src/main/java/de/uniks/pioneers/controller/MapTemplateViewController.java
@@ -0,0 +1,88 @@
+package de.uniks.pioneers.controller;
+
+import de.uniks.pioneers.App;
+import de.uniks.pioneers.Main;
+import de.uniks.pioneers.computation.LoadMapTemplate;
+import de.uniks.pioneers.model.Harbor;
+import de.uniks.pioneers.model.Map;
+import de.uniks.pioneers.model.Tile;
+import de.uniks.pioneers.template.HarborTemplate;
+import de.uniks.pioneers.template.MapTemplate;
+import de.uniks.pioneers.template.TileTemplate;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapTemplateViewController implements Controller {
+
+
+	@FXML
+	public Label mapNameLabel;
+	@FXML
+	public Button backButton;
+	@FXML
+	public ScrollPane mapPane;
+	private final App app;
+	private MapTemplatesScreenController mapTemplatesScreenController;
+	private final MapTemplate mapTemplate;
+
+	private Map map;
+
+	private LoadMapTemplate loadMapTemplate = new LoadMapTemplate();
+	private List<Tile> tiles = new ArrayList<>();
+	private List<Harbor> harbors = new ArrayList<>();
+
+	public MapTemplateViewController(App app, MapTemplate mapTemplate, MapTemplatesScreenController mapTemplatesScreenController) {
+		this.app = app;
+		this.mapTemplate = mapTemplate;
+		this.mapTemplatesScreenController = mapTemplatesScreenController;
+	}
+
+	@Override
+	public void init() {
+		for (TileTemplate tileTemplate: mapTemplate.tiles()) {
+			tiles.add(new Tile(tileTemplate.x(), tileTemplate.y(), tileTemplate.z(), tileTemplate.type(), tileTemplate.numberToken()));
+		}
+
+		for (HarborTemplate harborTemplate: mapTemplate.harbors()) {
+			harbors.add(new Harbor(harborTemplate.x(), harborTemplate.y(), harborTemplate.z(), harborTemplate.type(), harborTemplate.side()));
+		}
+		map = new Map(null,tiles, harbors);
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public Parent render() {
+		// load UI elements
+		final FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/MapTemplateView.fxml"));
+		loader.setControllerFactory(c -> this);
+		final Parent parent;
+		try {
+			parent = loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		this.mapPane.setContent(this.loadMapTemplate.loadMap(map, false));
+		this.mapNameLabel.setText(this.mapTemplate.name());
+
+		return parent;
+	}
+
+	public void backButtonPressed(ActionEvent actionEvent) {
+		this.app.show(mapTemplatesScreenController);
+	}
+}
